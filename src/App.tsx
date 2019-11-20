7ee2ff96ca32d9e9d081dec2feb110e259686a16
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-11-20 23:12:37 +0700

    add router, login, and not-found

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// styles
 import './App.scss';
+
+// pages
+import Login from  './modules/auth/login';
+import NotFound from './modules/auth/not-found/not-found';
+
+// 3rd lib
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Home: React.FC = () => {
+  return (
+    <div>Home</div>
+  )
+}
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      TES
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
