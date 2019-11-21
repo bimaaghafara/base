15b4e5a82c5dc0194223049a892b83e4dbfe8e99
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-11-21 23:29:47 +0700

    update login-login screen

--- src/modules/auth/login/login.tsx
+++ src/modules/auth/login/login.tsx
@@ -1,11 +1,88 @@
 import React from 'react';
+import './login.scss';
 
-const Login: React.FC = () => {
-  return (
-    <div>
-      Login
-    </div>
-  );
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+class NormalLoginForm extends React.Component<any, any> {
+
+  state = {
+    show: false
+  };
+
+  componentDidMount() {
+    console.log(this.state.show);
+    setTimeout(() => {
+      this.setState({show:true}, 
+        () => console.log(this.state.show));
+    }, 5000);
+  }
+
+  handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.props.form.validateFields((err: any, values: any) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  
+  LoginLogin = () => {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Passwordddd"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    );
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const LoginLogin = this.LoginLogin;
+
+    return (
+      <div id="login-page">
+        {this.state.show &&
+          <LoginLogin></LoginLogin>
+        }
+      </div>
+    );
+  }
 }
+
+const Login = Form.create({ name: 'normal_login' })(NormalLoginForm);
 
 export default Login;
