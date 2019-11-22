6d18b5b8f552cd4a64586ee70c5f9a00f66d229d
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-11-22 22:57:19 +0700

    onprogress create 2 forms

--- src/modules/auth/login/login.tsx
+++ src/modules/auth/login/login.tsx
@@ -1,21 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './login.scss';
 
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-class NormalLoginForm extends React.Component<any, any> {
-
+class LoginPassword extends React.Component<any, any> {
   state = {
-    show: false
+    LoginPassword: false
   };
-
-  componentDidMount() {
-    console.log(this.state.show);
-    setTimeout(() => {
-      this.setState({show:true}, 
-        () => console.log(this.state.show));
-    }, 5000);
-  }
 
   handleSubmit = (e: any) => {
     e.preventDefault();
@@ -26,63 +17,96 @@ class NormalLoginForm extends React.Component<any, any> {
     });
   };
 
-  
-  LoginLogin = () => {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Passwordddd"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(<Checkbox>Remember me</Checkbox>)}
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </Form.Item>
-      </Form>
-    );
-  }
-
   render() {
     const { getFieldDecorator } = this.props.form;
-    const LoginLogin = this.LoginLogin;
 
     return (
       <div id="login-page">
-        {this.state.show &&
-          <LoginLogin></LoginLogin>
-        }
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Passwordddd"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(<Checkbox>Remember me</Checkbox>)}
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
+  
 }
+const LoginPasswordForm = Form.create({ name: 'LoginPassword' })(LoginPassword);
 
-const Login = Form.create({ name: 'normal_login' })(NormalLoginForm);
+class LoginForm extends React.Component<any, any> {
 
-export default Login;
+  state = {
+    username: '',
+    password: 'passwordState'
+  };
+
+  componentDidMount() {
+  }
+
+  async setUsername(username: string) {
+    this.setState({username: username})
+  }
+
+  async setPassword(password: string) {
+    this.setState({password: password})
+  }
+
+  login(){
+    // call be service to login
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {!this.state.username &&
+          <div>
+            Login Password
+            <br/><br/>
+            <Button type="primary" onClick={ async () => {
+              await this.setPassword('dsfdsfdsf');
+              this.login();
+            }}>tesss</Button>
+            <Button type="primary" onClick={() => this.setUsername('usernameState')}>goToLoginPassword</Button>
+          </div>
+        }
+        {this.state.username &&
+          <LoginPasswordForm></LoginPasswordForm>
+        }
+      </Fragment>
+    );
+  }
+}
+export default LoginForm;
