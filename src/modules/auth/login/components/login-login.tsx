c4dfc5bdcab2ff054223f0e07e3cea51d286e9fc
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-11-26 23:03:00 +0700

    update .editorconfig, update hoc withLoginContext

--- src/modules/auth/login/components/login-login.tsx
+++ src/modules/auth/login/components/login-login.tsx
@@ -1,61 +1,55 @@
 
 import React, { Fragment } from 'react';
 import '../login.scss';
-import { LoginProvider, LoginConsumer } from '../login.context';
+import { LoginProvider, LoginConsumer, withLoginContext } from '../login.context';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
 class LoginLogin extends React.Component<any, any> {
-    constructor(props: any) {
-      super(props);
-      this.state = {};
-    }
-  
-    handleSubmit = (e: any) => {
-      e.preventDefault();
-      this.props.form.validateFields((err: any, values: any) => {
-        if (!err) {
-          console.log('Received values of form: ', values);
-        }
-      });
-    };
-  
-    render() {
-      const { getFieldDecorator } = this.props.form;
-  
-      return (
-        <Fragment>
-          <LoginConsumer>
-            {({visibleComponent, updateVisibleComponent}: any) => (
-              <Fragment>
-                <div id="login-page">
-                    <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Form.Item>
-                            {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
-                            })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />,
-                            )}
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button" onClick={() => {
-                                updateVisibleComponent(visibleComponent === 'LoginLogin'? 'LoginPassword' : 'LoginLogin')
-                            }}>
-                                Next
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-              </Fragment>
-            )}
-          </LoginConsumer>
-        </Fragment>
-      );
-    }
-    
-  }
-  
-  export const LoginLoginForm: any = Form.create({ name: 'LoginLogin' })(LoginLogin);
-  
+	constructor(props: any) {
+		super(props);
+		this.state = {};
+	}
+
+	handleSubmit = (e: any) => {
+		e.preventDefault();
+		this.props.form.validateFields((err: any, values: any) => {
+			if (!err) {
+				console.log('Received values of form: ', values);
+			}
+		});
+	};
+
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		const { loginContext } = this.props;
+
+		return (
+			<Fragment>
+				<div id="login-page">
+					<Form onSubmit={this.handleSubmit} className="login-form">
+						<Form.Item>
+							{getFieldDecorator('username', {
+								rules: [{ required: true, message: 'Please input your username!' }],
+							})(
+								<Input
+									prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+									placeholder="Username"
+								/>,
+							)}
+						</Form.Item>
+						<Form.Item>
+							<Button type="primary" htmlType="submit" className="login-form-button" onClick={() => {
+								loginContext.updateVisibleComponent(loginContext.visibleComponent === 'LoginLogin' ? 'LoginPassword' : 'LoginLogin')
+							}}>
+								Next!
+              </Button>
+						</Form.Item>
+					</Form>
+				</div>
+			</Fragment>
+		);
+	}
+
+}
+
+export const LoginLoginForm: any = Form.create({ name: 'LoginLogin' })(withLoginContext(LoginLogin));
--- src/modules/auth/login/login.context.tsx
+++ src/modules/auth/login/login.context.tsx
@@ -2,28 +2,40 @@ import React, { createContext } from 'react';
 
 // default value for LoginContext
 const LoginContext: any = createContext({
-  visibleComponent: 'LoginLogin',
-  updateVisibleComponent: () => {},
+	visibleComponent: 'LoginLogin',
+	updateVisibleComponent: () => { },
 });
 
 // provider
 export class LoginProvider extends React.Component {
-  updateVisibleComponent = (newVisibleComponent: 'LoginLogin' | 'LoginPassword') => {
-    this.setState({ visibleComponent: newVisibleComponent });
-  };
+	updateVisibleComponent = (newVisibleComponent: 'LoginLogin' | 'LoginPassword') => {
+		this.setState({ visibleComponent: newVisibleComponent });
+	};
 
-  state = {
-    visibleComponent: 'LoginLogin',
-    updateVisibleComponent: this.updateVisibleComponent,
-  };
+	state = {
+		visibleComponent: 'LoginLogin',
+		updateVisibleComponent: this.updateVisibleComponent,
+	};
 
-  render() {
-    return (
-      <LoginContext.Provider value={this.state}>
-        {this.props.children}
-      </LoginContext.Provider>
-    );
-  }
+	render() {
+		return (
+			<LoginContext.Provider value={this.state}>
+				{this.props.children}
+			</LoginContext.Provider>
+		);
+	}
 }
 
 export const LoginConsumer = LoginContext.Consumer;
+
+export const withLoginContext = (Component: any) => {
+	return (props: any) => {
+		return (
+			<LoginConsumer>
+				{(loginContext: any) => {
+					return <Component {...props} loginContext={loginContext} />;
+				}}
+			</LoginConsumer>
+		);
+	};
+};
