b1e603ddb1d89b4429b0272b202d7ae85d7dee70
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-11-28 22:09:39 +0700

    update create-your-account

--- src/modules/auth/create-account/components/create-your-account.tsx
+++ src/modules/auth/create-account/components/create-your-account.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './create-your-account.scss';
 
 // 3rd libs
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Icon, Input, Button, DatePicker, Select, AutoComplete } from 'antd';
 import { withRouter } from 'react-router-dom';
 
 class CreateYourAccountForm extends React.Component<any, any> {
@@ -40,9 +40,79 @@ class CreateYourAccountForm extends React.Component<any, any> {
 						{getFieldDecorator('lastName', {
 							rules: [{ required: true, message: 'Please input your last name!' }],
 						})(
-							<Input placeholder="last name"/>,
+							<Input placeholder="Last name"/>,
 						)}
 					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('userName', {
+							rules: [{ required: true, message: 'Please input your username!' }],
+						})(
+							<Input placeholder="Username"/>,
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('password', {
+							rules: [{required: true, message: 'Please input your password!'}],
+						})(
+							<Input.Password
+								prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}} />}
+								placeholder="Password"
+							/>
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('confrimPassword', {
+							rules: [{required: true, message: 'Please confirm your password!'}],
+						})(
+							<Input.Password
+								prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}} />}
+								placeholder="Confirm Password"
+							/>
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('phoneNumber', {
+							rules: [{ required: true, message: 'Please input your phone number!' }],
+						})(<Input placeholder="Phone Number"/>)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('date-picker', {
+							rules: [{ type: 'object', required: true, message: 'Please select time!' }],
+						})(<DatePicker placeholder="Date of Birth" style={{'width': '100%'}}/>)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('gender', {
+							rules: [{ required: true, message: 'Please select your gender!' }],
+						})(
+							<Select
+								placeholder="Select a Gender"
+							>
+								<Select.Option value="male">male</Select.Option>
+								<Select.Option value="female">female</Select.Option>
+							</Select>,
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('country', {
+							rules: [{ required: true, message: 'Please select your country!' }],
+						})(
+							<AutoComplete
+								dataSource={['USA', 'Indonesia', 'Singapore', 'China']}
+								placeholder="Country"
+								filterOption={(inputValue: any, option:any) =>
+									option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+								}
+							/>
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator('Address', {
+							rules: [{ required: true, message: 'Please input your Address!' }],
+						})(
+							<Input.TextArea placeholder="Address" rows={3}/>,
+						)}
+					</Form.Item>
+
 					<Form.Item>
 						<div style={{clear: 'both'}}>
 							<Button
