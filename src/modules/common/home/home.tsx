6ef41ba60b0292ce9603a899e23f3f08c5bab5a9
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-12-03 00:20:08 +0700

    update sidebar

---
+++ src/modules/common/home/home.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+// styles
+import './home.scss';
+
+// 3rd libs
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+
+class Home extends React.Component<any, any> {
+
+	state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+  
+  render() {
+
+    return (
+			<div id="home">
+			<Layout>
+				<Sider
+					trigger={null}
+					collapsible
+					collapsed={this.state.collapsed}
+					collapsedWidth={0}
+					breakpoint="sm"
+					onBreakpoint={(islessThanBreakpoint) => {console.log(islessThanBreakpoint)}}
+				>
+					<div className="logo" />
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+						<Menu.Item key="1">
+							<Icon type="user" />
+							<span>nav 1</span>
+						</Menu.Item>
+						<Menu.Item key="2">
+							<Icon type="video-camera" />
+							<span>nav 2</span>
+						</Menu.Item>
+						<Menu.Item key="3">
+							<Icon type="upload" />
+							<span>nav 3</span>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout>
+					<Header style={{ background: '#fff', padding: 0 }}>
+						<Icon
+							className="trigger"
+							type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+							onClick={this.toggle}
+						/>
+					</Header>
+					<Content
+						style={{
+							margin: '24px 16px',
+							padding: 24,
+							background: '#fff',
+							minHeight: 1280,
+						}}
+					>
+						Content
+          </Content>
+				</Layout>
+			</Layout>
+			</div>
+		)
+  }
+}
+export default Home;
