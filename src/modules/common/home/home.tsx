dfca2fc1c24b69a74b6e4f5ce617c1ca77ec81f0
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-12-03 22:33:01 +0700

    update style and structure

--- src/modules/common/home/home.tsx
+++ src/modules/common/home/home.tsx
@@ -8,62 +8,91 @@ import { Layout, Menu, Icon } from 'antd';
 const { Header, Sider, Content } = Layout;
 
 
+const SideNavigation = (props: any) => {
+	const {collapsed, toggleSideNavigation, onBreakpoint} = props;
+	return (
+		<Sider
+			className='side-navigation'
+			theme="dark"
+			width="100%"
+			trigger={null}
+			collapsible
+			collapsed={collapsed}
+			collapsedWidth={0}
+			breakpoint="md"
+			onBreakpoint={(islessThanBreakpoint) => {onBreakpoint(islessThanBreakpoint)}}
+		>
+			<Icon
+				className="toggle-side-navigation close-menu"
+				type={collapsed ? 'menu-unfold' : 'menu-fold'}
+				onClick={toggleSideNavigation}
+			/>
+			<div className="logo"/>
+			<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+				<Menu.Item key="1">
+					<Icon type="user" />
+					<span>nav 1</span>
+				</Menu.Item>
+				<Menu.Item key="2">
+					<Icon type="video-camera" />
+					<span>nav 2</span>
+				</Menu.Item>
+				<Menu.Item key="3">
+					<Icon type="upload" />
+					<span>nav 3</span>
+				</Menu.Item>
+			</Menu>
+		</Sider>
+	);
+}
+
+const TopNavigation = (props: any) => {
+	const {collapsed, toggleSideNavigation} = props;
+	return (
+		<Header className="top-navigation">
+			<Icon
+				className="toggle-side-navigation"
+				type={collapsed ? 'menu-unfold' : 'menu-fold'}
+				onClick={toggleSideNavigation}
+			/>
+		</Header>
+	)
+}
+
 class Home extends React.Component<any, any> {
 
 	state = {
-    collapsed: false,
+    collapsed: true,
 	};
 
-  toggle = () => {
+  toggleSideNavigation = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     });
-  };
+	};
+
+	onBreakpoint = (islessThanBreakpoint: boolean) => {
+		// md: 768px
+		// this.setState({
+    //   collapsed: islessThanBreakpoint,
+    // });
+	}
   
   render() {
 
     return (
 			<div id="home">
 			<Layout style={{maxHeight:'100vh'}} >
-				<Sider
-					theme="dark"
-					width="100%"
-					trigger={null}
-					collapsible
+				<SideNavigation
 					collapsed={this.state.collapsed}
-					collapsedWidth={0}
-					breakpoint="sm"
-					onBreakpoint={(islessThanBreakpoint) => {console.log(islessThanBreakpoint)}}
-				>
-					<Icon
-						className="trigger close-menu"
-						type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-						onClick={this.toggle}
-					/>
-					<div className="logo"/>
-					<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-						<Menu.Item key="1">
-							<Icon type="user" />
-							<span>nav 1</span>
-						</Menu.Item>
-						<Menu.Item key="2">
-							<Icon type="video-camera" />
-							<span>nav 2</span>
-						</Menu.Item>
-						<Menu.Item key="3">
-							<Icon type="upload" />
-							<span>nav 3</span>
-						</Menu.Item>
-					</Menu>
-				</Sider>
+					toggleSideNavigation={this.toggleSideNavigation}
+					onBreakpoint={this.onBreakpoint}
+				/>
 				<Layout>
-					<Header style={{ background: '#fff', padding: 0 }}>
-						<Icon
-							className="trigger"
-							type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-							onClick={this.toggle}
-						/>
-					</Header>
+					<TopNavigation
+						collapsed={this.state.collapsed}
+						toggleSideNavigation={this.toggleSideNavigation}
+					/>
 					<Content
 						style={{
 							margin: '24px 16px',
