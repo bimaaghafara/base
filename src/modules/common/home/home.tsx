b98c351f9167e0105735e43b586cc7c0c55db495
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-12-04 22:16:27 +0700

    onprogress

--- src/modules/common/home/home.tsx
+++ src/modules/common/home/home.tsx
@@ -4,13 +4,79 @@ import React from 'react';
 import './home.scss';
 
 // 3rd libs
-import { Carousel, Card, Button } from 'antd';
+import { Carousel, Card, Button, Form, Modal, Input, Radio } from 'antd';
 import IqNavigation from '../../../shared/ui/iq-navigation/IqNavigation';
 
+const SubscriptionModal: any = Form.create({ name: 'form_in_modal' })(
+  class extends React.Component<any, any> {
+    render() {
+      const { visible, onCancel, onCreate, form } = this.props;
+      const { getFieldDecorator } = form;
+      return (
+        <Modal
+          visible={visible}
+          title="Create a new collection"
+          okText="Create"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form layout="vertical">
+            <Form.Item label="Title">
+              {getFieldDecorator('title', {
+                rules: [{ required: true, message: 'Please input the title of collection!' }],
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="Description">
+              {getFieldDecorator('description')(<Input type="textarea" />)}
+            </Form.Item>
+            <Form.Item className="collection-create-form_last-form-item">
+              {getFieldDecorator('modifier', {
+                initialValue: 'public',
+              })(
+                <Radio.Group>
+                  <Radio value="public">Public</Radio>
+                  <Radio value="private">Private</Radio>
+                </Radio.Group>,
+              )}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  },
+);
 
 class Home extends React.Component<any, any> {
 
 	state = {
+		visible: false
+	};
+
+	formRef: any;
+
+	showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handleCreate = () => {
+    const { form } = this.formRef.props;
+    form.validateFields((err: any, values: any) => {
+		if (err) {
+			return;
+		}
+
+		console.log('Received values of form: ', values);
+		form.resetFields();
+		this.setState({ visible: false });
+		});
+	};
+
+	saveFormRef = (formRef: any) => {
+		this.formRef = formRef;
 	};
   
   render() {
@@ -22,7 +88,7 @@ class Home extends React.Component<any, any> {
 						<div>
 							<Card className="transparent">
 								<h1>Win an Umrah</h1>
-								<Button type="primary" size="large">
+								<Button type="primary" size="large" onClick={this.showModal}>
 									Enroll me
 								</Button>
 								<Button type="danger" size="large">
@@ -40,6 +106,13 @@ class Home extends React.Component<any, any> {
 						</div>
 					</Carousel>
 				</div>
+
+				<SubscriptionModal
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
 			</IqNavigation>
 		)
 	}
