5d0d3022e2eaed03a8411c46834b2167d33f0988
Erlangga <Noting>
2020-01-06 00:57:33 +0700

    Update under.tsx

--- src/modules/others/classifieds/components/under/under.tsx
+++ src/modules/others/classifieds/components/under/under.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
 // 3rd libs
-import { Breadcrumb, Icon } from 'antd';
+import { Breadcrumb, Icon, Row, Col } from 'antd';
 
 export const Under: React.FC<any> = () => {
 	return (
 		<div className="title-wrapper">
-			<b>Posted Under : </b>
-                <Breadcrumb className="breadcrumb">
-                    <Breadcrumb.Item href="">
-                        <span>ALL</span>
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Item href="">
-                        <span>Job Listing</span>
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Item href="">
-                        <span>Job Post</span>
-                    </Breadcrumb.Item>
-                </Breadcrumb>
+			<Row>
+                <Col xs={10} md={6}><b>Posted Under: </b></Col>
+                <Col xs={14} md={18}>
+                    <Breadcrumb>
+                        <Breadcrumb.Item href="">
+                            <span>ALL</span>
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Item href="">
+                            <span>Job Listing</span>
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Item href="">
+                            <span>Job Post</span>
+                        </Breadcrumb.Item>
+                    </Breadcrumb>
+                </Col>
+            </Row>
 		</div>
 	)
 }
