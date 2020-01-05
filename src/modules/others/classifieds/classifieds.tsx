8448cdc9353545bc9ad9bfa39f62db46c72b3a34
Erlangga <Noting>
2020-01-05 23:25:16 +0700

    Revert "123213"
    This reverts commit 2b608b43bc1cec8b04f0388feed2cea30acba83b.

--- src/modules/others/classifieds/classifieds.tsx
+++ src/modules/others/classifieds/classifieds.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
 import './classifieds.scss';
-
-// components
 import { IqNavigation } from '../../../shared/ui/iq-navigation/iq-navigation';
-import { ClassifiedsList } from './classifieds-list/classifieds-list';
 
-// 3rd libs
-import { Route, Redirect, Switch } from 'react-router-dom';
 
-class Locals extends React.Component<any, any> {
-	render() {
-		return (
+class Classifieds extends React.Component<any, any> {
+
+	state = {
+	}
+
+  componentDidMount() {
+  }
+  
+  render() {
+    return (
 			<IqNavigation>
-				<div id="locals-page">
-					<Switch>
-						<Route
-							exact path={`${this.props.match.url}`}
-							render={() => (<ClassifiedsList />)}
-						/>
-						{/* <Route
-							path={`${this.props.match.url}/details/:id`}
-							render={() => (<LocalsDetails />)}
-						/> */}
-						<Redirect to={`/not-found?url=${this.props.location.pathname}`} />
-					</Switch>
+				<div id="classifieds-page">
+					classifieds
 				</div>
 			</IqNavigation>
-		);
-	}
+    );
+  }
 }
-
-export default Locals;
+export default Classifieds;
