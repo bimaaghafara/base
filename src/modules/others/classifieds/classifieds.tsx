dcda93a79dca724d75ff3c0c78bbffe22f01fb93
Erlangga <Noting>
2020-01-05 23:32:01 +0700

    Classifieds List
    Classifieds  List

--- src/modules/others/classifieds/classifieds.tsx
+++ src/modules/others/classifieds/classifieds.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './classifieds.scss';
 import { IqNavigation } from '../../../shared/ui/iq-navigation/iq-navigation';
+import { ClassifiedsList } from './classifieds-list/classifieds-list';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 
 class Classifieds extends React.Component<any, any> {
-
-	state = {
-	}
-
-  componentDidMount() {
-  }
-  
-  render() {
-    return (
+	render() {
+		return (
 			<IqNavigation>
-				<div id="classifieds-page">
-					classifieds
+				<div id="locals-page">
+					<Switch>
+						<Route
+							exact path={`${this.props.match.url}`}
+							render={() => (<ClassifiedsList />)}
+						/>
+						{/* <Route
+							path={`${this.props.match.url}/details/:id`}
+							render={() => (<LocalsDetails />)}
+						/> */}
+						<Redirect to={`/not-found?url=${this.props.location.pathname}`} />
+					</Switch>
 				</div>
 			</IqNavigation>
     );
---
+++ src/modules/others/classifieds/components/search/search.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// 3rd libs
+import { Row, Col, Input, Button } from 'antd';
+
+export const Search: React.FC<any> = () => {
+	return (
+		<div className="directory-wrapper">
+			<Row gutter={[24, 24]}>
+                <Col md={6} style={{alignItems: 'center', alignContent: 'center', }}>
+                    <b>Search Classifieds</b>
+                </Col>
+				<Col md={6}>
+                    <Input.Search
+                        placeholder="What? (Shools,Mosques,etc)"
+                        onSearch={value => '50%'}
+                    />
+				</Col>
+                <Col md={6}>
+                    <Input.Search
+                        placeholder="Where? (City, Zip)"
+                        onSearch={value => ''}
+                    />
+                </Col>
+                <Col md={6}>
+                    <Button type="primary" icon="search">
+                        Search
+                    </Button>
+                </Col>
+			</Row>
+		</div>
+	)
+}
