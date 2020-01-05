2b608b43bc1cec8b04f0388feed2cea30acba83b
Erlangga <Noting>
2020-01-05 23:25:03 +0700

    123213
    12321

--- src/modules/others/classifieds/classifieds.tsx
+++ src/modules/others/classifieds/classifieds.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import './classifieds.scss';
+
+// components
 import { IqNavigation } from '../../../shared/ui/iq-navigation/iq-navigation';
+import { ClassifiedsList } from './classifieds-list/classifieds-list';
 
+// 3rd libs
+import { Route, Redirect, Switch } from 'react-router-dom';
 
-class Classifieds extends React.Component<any, any> {
-
-	state = {
-	}
-
-  componentDidMount() {
-  }
-  
-  render() {
-    return (
+class Locals extends React.Component<any, any> {
+	render() {
+		return (
 			<IqNavigation>
-				<div id="classifieds-page">
-					classifieds
+				<div id="locals-page">
+					<Switch>
+						<Route
+							exact path={`${this.props.match.url}`}
+							render={() => (<ClassifiedsList />)}
+						/>
+						{/* <Route
+							path={`${this.props.match.url}/details/:id`}
+							render={() => (<LocalsDetails />)}
+						/> */}
+						<Redirect to={`/not-found?url=${this.props.location.pathname}`} />
+					</Switch>
 				</div>
 			</IqNavigation>
-    );
-  }
+		);
+	}
 }
-export default Classifieds;
+
+export default Locals;
